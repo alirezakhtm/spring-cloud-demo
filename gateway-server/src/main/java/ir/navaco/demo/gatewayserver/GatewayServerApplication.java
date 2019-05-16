a58481f0d626aecd5a0096efd247279d41be7a0c
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-05-16 15:33:40 +0430

    In progressing for adding gateway to project ...

---
+++ gateway-server/src/main/java/ir/navaco/demo/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package ir.navaco.demo.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
--- ui-server/src/main/java/com/navaco/demo/uiserver/UiServerApplication.java
+++ ui-server/src/main/java/com/navaco/demo/uiserver/UiServerApplication.java
@@ -2,10 +2,12 @@ package com.navaco.demo.uiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaClient
