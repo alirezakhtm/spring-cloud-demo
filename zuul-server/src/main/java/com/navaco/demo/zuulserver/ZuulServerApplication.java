1ebcea57f956fc6f93f394aeaee740246e3053a2
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-05-17 21:05:38 +0430

    Zuul Server module has been added to project.

---
+++ zuul-server/src/main/java/com/navaco/demo/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.navaco.demo.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+}
