af8e127a2df1029c4140d7eb761ddeb9b0b24365
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-05-17 22:13:13 +0430

    In progressing for adding gateway to project ...

---
+++ zuul-server/src/main/java/com/navaco/demo/zuulserver/MyPreZuulFilter.java
@@ -0,0 +1,37 @@
+package com.navaco.demo.zuulserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyPreZuulFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest httpServletRequest = requestContext.getRequest();
+
+
+        return null;
+    }
+}
--- zuul-server/src/main/java/com/navaco/demo/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/navaco/demo/zuulserver/ZuulServerApplication.java
@@ -2,8 +2,12 @@ package com.navaco.demo.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ZuulServerApplication {
 
     public static void main(String[] args) {
